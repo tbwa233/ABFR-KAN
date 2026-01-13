11a3717f1aec48b2cc512c19181396d049833fe7
Tyler Ward <tbwa233@uky.edu>
2026-01-13 14:51:46 -0500

    Add files via upload

---
+++ ABFR-KANv2/Code/NYU_dataset.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+import heapq
+
+class GDataset(Dataset):
+
+    def __init__(self, idxs, csv):
+        super(GDataset, self).__init__()
+        self.fc_matrix_dot = os.path.join('data')
+        self.idxs = idxs
+        self.csv = csv
+
+    def __len__(self):
+        return self.idxs.shape[0]
+
+    def __getitem__(self, item):
+        index = self.idxs[item]
+        file_name = self.csv['name'].iloc[index]
+        label = self.csv['label'].iloc[index].astype(int)
+
+        fc_matrix_path = os.path.join(self.fc_matrix_dot, 'FCmatrix_random_iterative', str(file_name), f'fcmatrix.npy')
+        position_path = f'./data/subjectwise_position_random_iterative/{file_name}.npy'
+
+        
+        """
+        # DEBUG: print which file is being loaded
+        if item == 0:  # only print for the first item (avoids huge spam)
+            print("Loading FC matrix from:", fc_matrix_path)
+            print("Loading position from:", position_path)
+        """
+        
+
+        fc_matrix0 = np.load(fc_matrix_path).astype(np.float32)
+        # print("Mean of fc_matrix:", fc_matrix0.mean(), "Std:", fc_matrix0.std())
+        fc_matrix = np.abs(fc_matrix0)
+        position = np.load(position_path).astype(np.float32)
+
+        return torch.tensor(fc_matrix[:256, :]), torch.tensor(label), torch.tensor(position[:256, :])
+
+def get_data_loader(i_fold):
+
+    df = pd.read_csv('data/UM_5fold.csv')
+
+    df = df.reset_index()
+    train_idxs = np.where(df['fold'] != i_fold)[0]
+    val_idxs = np.where(df['fold'] == i_fold)[0]
+
+    TrainDataset = GDataset(train_idxs, df)
+    ValDataset = GDataset(val_idxs, df)
+    TrainLoader = DataLoader(TrainDataset, batch_size=16, shuffle=True, drop_last=True)
+    ValLoader = DataLoader(ValDataset, batch_size=1)
+    return TrainLoader, ValLoader
+
+if __name__ == '__main__':
+    TrainLoader, ValLoader = get_data_loader(0)
+    for fc, crs, posi in TrainLoader:
+        print(fc.shape, crs.shape, posi.shape)
+        break
+    print(' ')
+    for fc, crs, posi in ValLoader:
+        print(fc.shape, crs.shape, posi.shape)
+        break
